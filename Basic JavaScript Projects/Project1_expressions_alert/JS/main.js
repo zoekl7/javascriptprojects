711e9373532e1d402a812fd41a6849a40feb0c1b
Zoe <zoeoklein@gmail.com>
2020-11-27 21:43:09 -0800

    More practice
    Practicing basics

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,4 +1,12 @@
 window.alert("Hello, World!");
-document.writer('Hello, world!');
-var J = "A string"
-document.write(J)
+
+document.write("Remember remember the 5th of November, \"the gun powder treason and plot./"
++ " I know of no reason why the Gunpowder Treason should ever be forgot ");
+
+// variables with a red font // 
+var Family = "Vendetta", V = "Vendetta", E = "Evey", D = "Delia";
+var V = V.fontcolor("red")
+var E = E.fontcolor("orange")
+document.write(V);
+
+document.write( 3 + 7 );
