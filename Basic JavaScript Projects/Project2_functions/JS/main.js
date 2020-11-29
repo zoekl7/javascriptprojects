0160172f6053c3ae15c6d9de64e5d922016669e8
Zoe <zoeoklein@gmail.com>
2020-11-29 14:16:35 -0800

    Update main.js

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -1,7 +1,9 @@
+//defines 'sentence' variable as a string, concatenates more text to it
 function myFunction() {
     document.getElementById("demo").innerHTML = "Practice";
 }
 
+//displays value of the "sentence" variable at the HTML element w/ ID "Concatenate"
 function myFunction() {
     var sentence = "Learning";
     sentence += " a lot";
