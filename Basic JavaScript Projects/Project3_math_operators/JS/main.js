ff53873a1b828f5a26f5bb3ee8a51ed3d5450d32
Zoe <zoeoklein@gmail.com>
2020-11-30 14:15:45 -0800

    Still working
    still working on this

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -18,5 +18,19 @@ function addition_Function() {
 
 function subtraction_Function() {
     var Subtraction = 7 - 3;
-    document.getElementById("Math").innerHTML = "7 - 3 = " + Subtraction;
+    document.getElementById("Math2").innerHTML = "7 - 3 = " + Subtraction;
+}
+
+//multiplication//
+function multiplication() {
+    var simple_Math = 7 * 3;
+    document.getElementById("Math3").innerHTML = "7 x 3 = " + simple_Math;
+}
+
+
+
+//multiple math operations at once//
+function more_Math() {
+    var simple_Math = (1 + 2) * 10 / 2 -5;
+    document.getElementById("Math").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + simple_Math;
 }
