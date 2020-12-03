ae767c10d88102a2decc714ae1d6237f2f70b824
Zoe <zoeoklein@gmail.com>
2020-12-02 16:23:34 -0800

    fixed
    Ride_Function fixed

--- Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
+++ Basic JavaScript Projects/Project6_ternary_operators_constructors/JS/main.js
@@ -1,7 +1,7 @@
 //ternary operation//
 function Ride_Function() {
     var Height, Can_ride;
-    Height = document.getElementById("Height").nodeValue;
+    Height = document.getElementById("Height").Value;
     Can_ride = (Height < 52) ? "You are too short":"You are tall enough";
     document.getElementById("Ride").innerHTML = Can_ride + "to ride.";
 }
---
+++ Basic JavaScript Projects/Project7_scope_time_function/js/main.js
@@ -0,0 +1,20 @@
+//Global and local variables //
+var x = 7;
+function add_1() {
+    document.write(13 + x + "<br>");
+}
+function add_2() {
+    document.write(x + 20);
+}
+add_1();
+add_2();
+
+function add_numbers1() {
+    var X = 10;
+    document.write(20 + X + "<br>");
+}
+function add_numbers2() {
+    document.write(X + 100);
+}
+add_numbers1();
+add_numbers2();
