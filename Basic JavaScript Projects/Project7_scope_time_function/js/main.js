07f7ceba4035a7d02e1aa9c8d1b28048204ed845
Zoe <zoeoklein@gmail.com>
2020-12-02 17:27:38 -0800

    Time Functions
    Time Functions

--- Basic JavaScript Projects/Project7_scope_time_function/js/main.js
+++ Basic JavaScript Projects/Project7_scope_time_function/js/main.js
@@ -18,3 +18,36 @@ function add_numbers2() {
 }
 add_numbers1();
 add_numbers2();
+
+//Get Date Method and if statements//
+function getYear() {
+    if (new Date().getMonth() > 11); {
+    document.getElementById("Holidays").innerHTML = "Happy Holidays!";
+    }
+}
+
+function Age_Function() {
+    Age = document.getElementById("Age").value;
+    if (Age >= 17) {
+        Movie = "Are are old enough!";
+    }
+    else {
+        Movie = "You are not old enough!";
+    }
+    document.getElementById("Movie_Age").innerHTML = Movie;
+}
+
+function Time_function() {
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time!";
+    }
+    else if (Time > 12 == Time < 18) {
+        Reply = "It is the afternoon.";
+    }
+    else {
+        Reply = "It is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
+}
