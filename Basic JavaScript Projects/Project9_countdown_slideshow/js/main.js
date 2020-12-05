b5477b9faba3731cd745a1a7c51f816de0a0055e
Zoe <zoeoklein@gmail.com>
2020-12-04 16:22:04 -0800

    slideshow

--- Basic JavaScript Projects/Project9_countdown_slideshow/js/main.js
+++ Basic JavaScript Projects/Project9_countdown_slideshow/js/main.js
@@ -11,3 +11,34 @@ function countdown() {
         }
     tick();
 }
+
+//Slide show //
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls//
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls//
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function Slides(n) {
+  var i;
+  var slideIndex = document.getElementsByClassName("Slides");
+  var dots = document.getElementsByClassName("squares");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace("active", "");
+  }
+  slides[slideindex-1].style.display = "block";
+  dots[slideIndex-1].className += "active";
+}
